f04117fe03395ce337c61e0f9b575193ad698377
Ashik224 <ashik.cse.pstu@gmail.com>
2021-09-03 00:31:18 +0600

    refactor project

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,37 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException, IOException {
-        TestCase1 testCase1 = new TestCase1();
+    static WebDriver driver;
+    static String browser;
+
+    public static void main(String[] args) throws Exception {
+        setBrowser();
+        driver = setBrowserConfig(browser);
+        runTest(driver);
+
+    }
+
+    public static void setBrowser() {
+        browser = "CHROME";
+    }
+
+    public static WebDriver setBrowserConfig(String browser) {
+        WebDriver driver = null;
+        if(browser.equals("CHROME")) {
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\BS0493\\OneDrive\\Documents\\SelSetup\\chromedriver.exe");
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
+
+    public static void runTest(WebDriver driver) throws Exception {
+        driver.get("https://qatest.marcombox.com/");
+        TestCase1 testCase1 = new TestCase1(driver);
         testCase1.init();
         testCase1.run();
+        driver.quit();
     }
 }
--- src/pages/asset/ShareFilePage.java
+++ src/pages/asset/ShareFilePage.java
@@ -1,13 +1,16 @@
-package pages;
+package pages.asset;
 
 import helpers.TestHelper;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 public class ShareFilePage {
     WebDriver driver;
     TestHelper helper;
 
     public ShareFilePage(WebDriver driver, TestHelper helper) {
-
+        this.driver = driver;
+        this.helper = helper;
+        PageFactory.initElements(driver, this);
     }
 }
